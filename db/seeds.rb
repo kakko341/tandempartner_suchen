622cef9fd07c9fe65398d8a9b1dee58a90e05e7d
kakko341 <riimoh3@yahoo.co.jp>
2019-05-28 09:15:01 +0000

    Bootstrap design

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(11..100).each do |number|
+    Message.create(title: 'test ' + number.to_s, content: 'message ' + number.to_s)
+end
