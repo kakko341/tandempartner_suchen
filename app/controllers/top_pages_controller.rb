dd844b1a80761e21580145a736dcb1ee36b5af24
kakko341 <riimoh3@yahoo.co.jp>
2019-08-10 15:29:13 +0000

    modify seed

--- app/controllers/top_pages_controller.rb
+++ app/controllers/top_pages_controller.rb
@@ -3,15 +3,10 @@ class TopPagesController < ApplicationController
   def home
     if logged_in?
       @user = current_user
-<<<<<<< HEAD
       @message  = current_user.messages.build
       @feed_items = current_user.feed_messages.order('created_at DESC').page(params[:page]).search(params[:search])
     else
       @user = User.new
-=======
-      @message = current_user.messages.build
-      @messages = current_user.feed_messages.order('created_at DESC').page(params[:page])
->>>>>>> 72aa981102d525f0f93f0c887601db612ffc8fc3
     end
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -4,13 +4,12 @@ class Message < ApplicationRecord
         
     validates :content, presence: true, length: {maximum: 255}
     validates :title, presence: true, length: {maximum: 255}
-    mount_uploader :photo, PhotoUploader
-    validate :photo_size
+    # mount_uploader :photo, PhotoUploader
+    # validate :photo_size
     
     has_many :favorites, foreign_key: 'message_id', dependent: :destroy
     has_many :users, through: :favorites
     
-<<<<<<< HEAD
     def self.search(search)
         if search
           where(["title LIKE? or content LIKE?", "%#{search}%", "%#{search}%"])
@@ -19,14 +18,4 @@ class Message < ApplicationRecord
         end
     end
 
-=======
-    private
-    
-    def photo_size
-      if photo.size > 5.megabytes
-        errors.add(:picture, "should be less than 5MB")
-      end
-    end
-    
->>>>>>> 72aa981102d525f0f93f0c887601db612ffc8fc3
 end
